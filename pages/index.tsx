fd82d4969a6900218c0194632686d56796e8c58a
MdodoIng <mdodoyt2@gmail.com>
2022-12-14 22:42:02 +0530

    Changes to be committed: 	modified:   pages/index.tsx 	new file:   public/20220822_093622.png 	new file:   public/Untitled.png

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { modulesFinder } from '@/lib/helpers';
 import Layout from 'components/layout/layout';
 import Module from 'components/module/Module';
 import { usePathname, useSearchParams } from 'next/navigation';
+import React from 'react';
 
 const Home = (data: {
   sate: any | undefined;
@@ -10,6 +11,9 @@ const Home = (data: {
   const pathname = usePathname();
   const { site, page }: any = data;
   const pageName = modulesFinder(pathname)
+  const [first, setFirst] = React.useState();
+  const [isFirst, setIsFirst] = React.useState();
+  
 
   if (!pageName[0]) {
     return (
@@ -28,11 +32,29 @@ const Home = (data: {
 
     
    return (
+    <>
+    {
+      !first &&
+    <div className='inset-0 grid grid-row-2 fixed bg-black z-[50000]'>
+      <div className='w-[80%] flex justify-center items-center mx-auto flex-col border-[5px] rounded-3xl overflow-hidden'>
+    <h1 className='text-white text-h4'>find masterpice smile</h1>
+        <img src="20220822_093622.png" alt="" className='w-full object-contain' />
+        <button onClick={() => setFirst(true)} className='bg-white text-black px-10 py-5 rounded-xl mt-5'>ith</button>
+      </div>
+      <div className='w-[80%] flex justify-center items-center mx-auto flex-col border-[5px] rounded-3xl overflow-hidden'>
+        <img src="Untitled.png" alt="" className='w-full object-contain' />
+        <button onClick={() => alert('ijj tett anser adichu ini pinne vaa')} className='bg-white text-black px-10 py-5 rounded-xl mt-5'>ith</button>
+      </div>
+
+    </div>}
+
     <Layout site={site} page={page}>
       {pageName[0].modules.map((page: { id: any; pageType: any; }) => (
         <Module key={page.id} data={page?.pageType} />
-      ))}
+        ))}
     </Layout>
+
+        </>
   );
 };
 
