f07d7252db64afb02b58f8be99e7545c22e1234d
MdodoIng <mdodoyt2@gmail.com>
2022-12-14 22:56:00 +0530

    modified:   pages/_app.tsx 	modified:   pages/index.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ import { LazyMotion, domAnimation, AnimatePresence } from 'framer-motion';
 
 import '/styles/globals.css';
 import { isBrowser, useScrollRestoration } from '@/lib/helpers';
-import { useContext, useEffect, useMemo } from 'react';
+import React, { useContext, useEffect, useMemo } from 'react';
 import { Router } from 'next/router';
 import { pageTransitionSpeed } from 'utils/motion';
 
@@ -33,6 +33,7 @@ const Site = ({ Component, pageProps, router }: any): JSX.Element => {
   const togglePageTransition = useTogglePageTransition();
   const { isPageTransition } = useSiteContext();
   const { data } = pageProps;
+  
 
   //  get scroll potion
   useScrollRestoration(router, isPageTransition);
@@ -93,13 +94,48 @@ const Site = ({ Component, pageProps, router }: any): JSX.Element => {
   );
 };
 
- function MyApp({ Component, pageProps, router }: any) {
+function MyApp({ Component, pageProps, router }: any) {
+  const [first, setFirst] = React.useState();
   const { data } = pageProps;
   return (
     <>
       <Head>
         <title>Next.js</title>
       </Head>
+      {!first && (
+        <div className="grid-row-2 fixed inset-0 z-[500000000000] grid bg-black">
+          <div className="mx-auto flex w-[80%] flex-col items-center justify-center overflow-hidden rounded-3xl border-[5px]">
+            <h1 className="text-h4 text-white">find masterpice smile</h1>
+            <a href='/' className='text-white text-h4 border p-5 bg-red-700 absolute '>onnum kannllagil ithil kick adichalla</a>
+            <img
+              src="20220822_093622.png"
+              className="w-full object-contain z-20"
+            />
+            <button
+              onClick={() => {
+                if (
+                  window.confirm(
+                    'ijj parajjath sattyam annnnu tonnu thalkaalam site kandu pettnnu povaan nook',
+                  )
+                )
+                  setFirst(true);
+              }}
+              className="mt-5 rounded-xl bg-white px-10 py-5 text-black"
+            >
+              ith
+            </button>
+          </div>
+          <div className="mx-auto flex w-[80%] flex-col items-center justify-center overflow-hidden rounded-3xl border-[5px]">
+            <img src="Untitled.png" alt="" className="w-full object-contain" />
+            <button
+              onClick={() => alert('ijj tett anser adichu ini pinne vaa')}
+              className="mt-5 rounded-xl bg-white px-10 py-5 text-black"
+            >
+              ith
+            </button>
+          </div>
+        </div>
+      )}
 
       <SiteContextProvider data={{ ...data }}>
         <Site Component={Component} pageProps={pageProps} router={router} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -33,24 +33,7 @@ const Home = (data: {
     
    return (
     <>
-    {
-      !first &&
-    <div className='inset-0 grid grid-row-2 fixed bg-black z-[50000]'>
-      <div className='w-[80%] flex justify-center items-center mx-auto flex-col border-[5px] rounded-3xl overflow-hidden'>
-    <h1 className='text-white text-h4'>find masterpice smile</h1>
-        <img src="20220822_093622.png" alt="" className='w-full object-contain' />
-        <button onClick={() =>{ 
-          if (window.confirm('ijj parajjath sattyam annnnu tonnu thalkaalam site kandu pettnnu povaan nook'))
-          setFirst(true)
-          
-          }} className='bg-white text-black px-10 py-5 rounded-xl mt-5'>ith</button>
-      </div>
-      <div className='w-[80%] flex justify-center items-center mx-auto flex-col border-[5px] rounded-3xl overflow-hidden'>
-        <img src="Untitled.png" alt="" className='w-full object-contain' />
-        <button onClick={() => alert('ijj tett anser adichu ini pinne vaa')} className='bg-white text-black px-10 py-5 rounded-xl mt-5'>ith</button>
-      </div>
-
-    </div>}
+   
 
     <Layout site={site} page={page}>
       {pageName[0].modules.map((page: { id: any; pageType: any; }) => (
