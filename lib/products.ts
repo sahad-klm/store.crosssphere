1053c72a20291bba1d6ce74e0e13b60e714d23c2
sahad-klm <msahad736@gmail.com>
2023-01-10 12:30:18 +0530

    modified:   lib/products.ts

--- lib/products.ts
+++ lib/products.ts
@@ -18,7 +18,7 @@ export const productsData = [
 		bacPicture:
 			'/spices/cardamom.png',
 		smallDetail:
-			'Uninhibited carnally hired played in whimpered dear gorilla koala depending and much yikes off far quetzal goodness and from for grimaced goodness unaccountably and meadowlark near unblushingly crucial scallop tightly neurotic hungrily some and dear furiously this apart.',
+		'Are you looking for a natural remedy to reduce stress, depression, and anxiety? Cardamom oil is your answer! Our 100% pure and therapeutic-grade cardamom oil is extracted from the finest cardamom plants in India. It is used for aromatherapy and can be applied topically or taken internally to help tackle a variety of ailments. Let our cardamom oil help you relax, reduce stress, and improve your well-being today!',
 		details: [
 			{
 				id: 1,
@@ -28,13 +28,13 @@ export const productsData = [
 						id: 1,
 						title: '',
 						detail:
-							'Uninhibited carnally hired played in whimpered dear gorilla koala depending and much yikes off far quetzal goodness and from for grimaced goodness unaccountably and meadowlark near unblushingly crucial scallop tightly neurotic hungrily some and dear furiously this apart. <br/> Spluttered narrowly yikes left moth in yikes bowed this that grizzly much hello on spoon-fed that alas rethought much decently richly and wow against the frequent fluidly at formidable acceptably flapped beside and much circa far over the bucolically hey precarious goldfinch mastodon goodness gnashed a jellyfish and one however because.',
+							'Are you looking for a natural way to improve your overall health? If so, then cardamom oil is the perfect solution! Our cardamom oil is extracted from the cardamom plant and infused with natural oils like coconut and jojoba. It is known to reduce stress, improve digestion, and boost your immune system. Not only that, but it has a wonderful aroma that can uplift the mood of any room. Try our cardamom oil today and experience the difference!',
 					},
 					{
 						id: 2,
 						title: 'Packaging & Delivery',
 						detail:
-							'Less lion goodness that euphemistically robin expeditiously bluebird smugly scratched far while thus cackled sheepishly rigid after due one assenting regarding censorious while occasional or this more crane went more as this less much amid overhung anathematic because much held one exuberantly sheep goodness so where rat wry well concomitantly. ~ Scallop or far crud plain remarkably far by thus far iguana lewd precociously and less rattlesnake contrary caustic wow this near, alas and next and pled the yikes articulate about as less cackled dalmatian in much less well jeering for the thanks blindly sentimental whimpered less across objectively fanciful grimaced wildly some wow and rose jeepers outgrew lugubrious luridly irrationally attractively dachshund.',
+							'Attention, wellness seekers! Are you looking for a natural way to reduce anxiety and stress? Look no further than cardamom oil. Our high-quality, 100% pure cardamom oil is derived from the seeds of the cardamom plant and is known to have soothing and calming effects on the body and mind. Not only that, it has a pleasant aroma which helps to boost your mood and energy levels. With our product, you can enjoy all the benefits of cardamom oil without',
 					},
 					{
 						id: 3,
