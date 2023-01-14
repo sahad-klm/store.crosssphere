3b236666d3e5ddb03f15a5392cfa094cdac97508
MdodoIng <mdodoyt2@gmail.com>
2023-01-14 15:39:49 +0530

    modified:   components/home/HomeComponent.tsx

--- components/home/HomeComponent.tsx
+++ components/home/HomeComponent.tsx
@@ -14,9 +14,9 @@ import Image from 'next/image';
 
 const HomeComponent = (): any => {
 
-  
+
   return (
-  <div className="h-ofHome relative z-10 flex items-center justify-center overflow-hidden px-4 md:h-[590px]  md:pt-7">
+    <div className="h-ofHome relative z-10 flex items-center justify-center overflow-hidden px-4 md:h-[590px]  md:pt-7">
       <Swiper
         modules={[Navigation, Pagination, Scrollbar, Autoplay]}
         slidesPerView={1}
@@ -35,7 +35,9 @@ const HomeComponent = (): any => {
               key={item.id}
               style={{ width: '100%', height: '100%' }}
             >
-              <div className="relative z-auto grid h-full content-center overflow-hidden rounded-3xl p-10 md:pl-20">
+
+              {/*  not in mobile  okay ithil ijj vallatum kalikkan nikkanda */}
+              <div className="relative z-auto md:grid hidden h-full content-center overflow-hidden rounded-3xl pl-20">
                 <Image fill loading='eager' blurDataURL={item.picture}
                   className="absolute top-0 left-0 -z-10 h-full w-full object-cover"
                   src={item.picture}
@@ -46,7 +48,7 @@ const HomeComponent = (): any => {
                   whileInView="show"
                   viewport={{ once: false, amount: 0.25 }}
                   variants={textVariant(0.2, 0.5)}
-                  className="max-w-[780px] font-body text-[42px] font-bold leading-10 text-gray-800 md:text-7xl"
+                  className="max-w-[780px] font-body font-bold leading-10 text-gray-800 text-7xl"
                 >
                   {item.title}
                 </motion.h1>
@@ -55,7 +57,7 @@ const HomeComponent = (): any => {
                   initial="hidden"
                   whileInView="show"
                   viewport={{ once: false, amount: 0.25 }}
-                  className="mt-5 font-body text-h6 leading-9 text-gray-500 md:text-h4"
+                  className="mt-5 font-body leading-9 text-gray-500 text-h4"
                 >
                   {item.subtitle}
                 </motion.h6>
@@ -64,9 +66,9 @@ const HomeComponent = (): any => {
                   initial="hidden"
                   whileInView="show"
                   viewport={{ once: false, amount: 0.25 }}
-                  className="relative mt-7 flex h-16 w-[279px] flex-row  items-center overflow-hidden rounded-full bg-white sm:mt-10 md:w-[450px]"
+                  className="relative mt-7 flex h-16 flex-row  items-center overflow-hidden rounded-full bg-white sm:mt-10 w-[450px]"
                 >
-                  <PaperPlaneIcon className="ml-1 mr-2 h-4 fill-gray-500 md:ml-6 md:mr-4" />
+                  <PaperPlaneIcon className=" h-4 fill-gray-500 ml-6 mr-4" />
                   <input
                     type="text"
                     placeholder="Your email address"
@@ -76,6 +78,44 @@ const HomeComponent = (): any => {
                     Subscribe
                   </button>
                 </motion.form>
+              </div>
+
+
+              {/* ith mobile no animation no  algram okay ithul onnum onnum punnakkum indakkan nikkanda */}
+
+
+              <div className="relative z-auto grid md:hidden h-full content-center overflow-hidden rounded-3xl p-10">
+                <Image fill loading='eager' blurDataURL={item.picture}
+                  className="absolute top-0 left-0 -z-10 h-full w-full object-cover"
+                  src={item.picture}
+                  alt="wallpaper"
+                />
+                <h1
+
+                  className="max-w-[780px] font-body text-[42px] font-bold leading-10 text-gray-800"
+                >
+                  {item.title}
+                </h1>
+                <h6
+
+                  className="mt-5 font-body text-h6 leading-9 text-gray-500"
+                >
+                  {item.subtitle}
+                </h6>
+                <form
+
+                  className="relative mt-7 flex h-16 w-[279px] flex-row  items-center overflow-hidden rounded-full bg-white "
+                >
+                  <PaperPlaneIcon className="ml-2 mr-2 h-4 fill-gray-500 " />
+                  <input
+                    type="text"
+                    placeholder="Your email address"
+                    className="h-full font-body text-sm tracking-wide text-gray-700 outline-none"
+                  />
+                  <button name='button' className="absolute -right-1 h-full rounded-full bg-emerald-500 px-5 font-body  text-sm tracking-wider text-white outline-none transition hover:bg-emerald-600 ">
+                    Subscribe
+                  </button>
+                </form>
               </div>
             </SwiperSlide>
           ),
