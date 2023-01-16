e44b195081088d255b4fd1da6b05640139bf5d76
sahad-klm <msahad736@gmail.com>
2023-01-16 16:02:19 +0530

    modified:   components/Header/Header.tsx 	new file:   public/logo.jpg

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -76,8 +76,8 @@ function Header(): any {
         <div className="flex h-[105px] w-full items-center justify-between border-b-[1px] border-solid border-gray-700 border-opacity-20 bg-white py-7 sm:px-4">
           <Link aria-label="Read more" href="/" className="flex h-full items-center justify-center">
             <Image width={176} height={100} loading='lazy' alt=''
-              src="/map1.webp"
-              className="h-full w-44 shrink-0  object-cover"
+              src="/logo.jpg"
+              className="h-144 w-66 shrink-0  object-cover"
             />
           </Link>
           <SearchBar
@@ -148,8 +148,8 @@ function Header(): any {
           />
           <Link aria-label="Read more" href="/" className="flex h-full items-center justify-center">
             <Image width={176} height={1000} alt='' loading='lazy'
-              src="/map1.webp"
-              className=" h-full w-44 shrink-0 cursor-pointer bg-red-300 object-contain"
+              src="/logo.jpg"
+              className=" h-full w-66 shrink-0 cursor-pointer bg-white object-contain"
             />
           </Link>
           <div>
