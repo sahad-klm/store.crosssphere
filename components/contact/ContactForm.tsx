429d419e1cda87c806bb91701ed3c839c678961d
MdodoIng <mdodoyt2@gmail.com>
2023-02-02 13:20:07 +0530

    modified:   components/contact/ContactForm.tsx

--- components/contact/ContactForm.tsx
+++ components/contact/ContactForm.tsx
@@ -1,8 +1,41 @@
 import { motion } from 'framer-motion';
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
+import emailjs from '@emailjs/browser';
+
+const initialState = {
+  name: '',
+  email: '',
+  phone: '',
+  subject: '',
+  message: '',
+};
 
 const ContactForm = () => {
+  const [formValue, setFormValue] = useState(initialState);
+
+  const handleSubmit = (element: React.FormEvent) => {
+    element.preventDefault()
+    const valueIsMoreThanZero =
+      formValue.email.length > 0 &&
+      formValue.email.length &&
+      formValue.message.length &&
+      formValue.name.length &&
+      formValue.phone.length &&
+      formValue.subject.length;
+
+    if (valueIsMoreThanZero) {
+      emailjs.send('service_8iz45q8', 'template_kebta6g',   formValue ,"1Iz_fZjNb2r4dgs7d").then(
+        function (response) {
+          console.log('SUCCESS!', response.status, response.text);
+          setFormValue(initialState);
+        },
+        function (error) {
+          console.log('FAILED...', error);
+        },
+      );
+
+  }; }
   return (
     <div className="mt-24 flex w-full flex-row justify-between gap-12">
       <div className="grid flex-1 auto-rows-max ">
@@ -78,7 +111,11 @@ const ContactForm = () => {
             *
           </motion.p>
         </div>
-        <form action="" className="grid auto-rows-max gap-4 ">
+        <form
+          onSubmit={handleSubmit}
+          action=""
+          className="grid auto-rows-max gap-4 "
+        >
           <motion.div
             initial="hide"
             whileInView="show"
@@ -101,21 +138,37 @@ const ContactForm = () => {
             className="grid gap-4 sm:grid-cols-2"
           >
             <input
+              onChange={(e) =>
+                setFormValue({ ...formValue, name: e.target.value })
+              }
+              value={formValue.name}
               type="text"
               placeholder="Your Name"
               className="h-16 rounded-lg border py-[10px] pl-[20px] pr-[10px] text-base text-gray-500 outline-none"
             />
             <input
+              onChange={(e) =>
+                setFormValue({ ...formValue, email: e.target.value })
+              }
+              value={formValue.email}
               type="text"
               placeholder="Your Email"
               className="h-16 rounded-lg border py-[10px] pl-[20px] pr-[10px] text-base text-gray-500 outline-none"
             />
             <input
+              onChange={(e) =>
+                setFormValue({ ...formValue, phone: e.target.value })
+              }
+              value={formValue.phone}
               type="text"
               placeholder="Your Phone"
               className="h-16 rounded-lg border py-[10px] pl-[20px] pr-[10px] text-base text-gray-500 outline-none"
             />
             <input
+              onChange={(e) =>
+                setFormValue({ ...formValue, subject: e.target.value })
+              }
+              value={formValue.subject}
               type="text"
               placeholder="Subject"
               className="h-16 rounded-lg border py-[10px] pl-[20px] pr-[10px] text-base text-gray-500 outline-none"
@@ -140,6 +193,10 @@ const ContactForm = () => {
               ease: [0.16, 1, 0.3, 1],
               delay: 0.55,
             }}
+            onChange={(e) =>
+              setFormValue({ ...formValue, message: e.target.value })
+            }
+            value={formValue.message}
             placeholder="Message"
             className="h-36 rounded-lg border py-[10px] pl-[20px] pr-[10px] text-base text-gray-500 outline-none"
           />
@@ -161,8 +218,9 @@ const ContactForm = () => {
               duration: 0.2,
               ease: [0.16, 1, 0.3, 1],
               delay: 0.65,
-            }}
-            className="w-max rounded-lg border-none bg-slate-500 h-16 px-16 font-head text-2xl text-white outline-none duration-300 hover:bg-emerald-500"
+            }} 
+            onClick={handleSubmit}
+            className="h-16 w-max rounded-lg border-none bg-slate-500 px-16 font-head text-2xl text-white outline-none duration-300 hover:bg-emerald-500"
           >
             Send Message
           </motion.button>
@@ -189,7 +247,10 @@ const ContactForm = () => {
         }}
         className="hidden h-[50%] w-[30%] overflow-hidden rounded-xl lg:flex"
       >
-        <Image width={700} height={500} loading='lazy'
+        <Image
+          width={700}
+          height={500}
+          loading="lazy"
           src="/contact.png"
           alt=""
           className="h-full w-full object-cover "
