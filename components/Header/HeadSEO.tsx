c07ccddbfa4d53a8524623b9f24628db474a6c3f
dodo <110053481+MdodoIng@users.noreply.github.com>
2023-09-05 07:12:41 +0000

    modified:   components/Header/HeadSEO.tsx new file:   public/favicon.png

--- components/Header/HeadSEO.tsx
+++ components/Header/HeadSEO.tsx
@@ -14,7 +14,7 @@ const HeadSEO = ({ site = {}, page = {}, schema }: any) => {
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="format-detection" content="telephone=no" />
 
-      <link rel="icon" href="seo.png" sizes="any" />
+      <link rel="icon" href="favicon.png" sizes="any" />
 
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link
