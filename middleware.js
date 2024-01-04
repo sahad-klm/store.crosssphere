247e6dacf37e40cfc840a2cc6345540080970f53
sahad-klm <msahad736@gmail.com>
2024-01-04 21:07:42 +0530

    deleted:    middleware.js

--- middleware.js
+++
@@ -1,9 +0,0 @@
-// import { NextResponse } from 'next/server';
-
-// export function middleware(request) {
-//   if (request.nextUrl.host.startsWith('aldaninternational.com')) {
-//     return NextResponse.redirect(
-//       new URL('', request.url),
-//     );
-//   }
-// }
